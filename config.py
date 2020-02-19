2dd22d81afa2457c21847d67488faadd5415c12d
lrittie <lrittie@dalet.com>
2020-02-19 14:30:31 +0000

    fixed some issues when fetching tags

--- config.py
+++ config.py
@@ -8,11 +8,12 @@ frmt = '%Y-%m-%d %H:%M:%S'
 profiles= [""]
 
 # add your regions below see https://docs.aws.amazon.com/general/latest/gr/rande.html
-regions = ["us-east-1","us-east-2","us-west-1","us-west-2","ca-central-1","eu-central-1","eu-west-1","eu-west-2","eu-west-3","eu-north-1","ap-east-1","ap-northeast-1","ap-northeast-2","ap-southeast-1","ap-southeast-2","ap-south-1","sa-east-1"]
+#regions = ["us-east-1","us-east-2","us-west-1","us-west-2","ca-central-1","eu-central-1","eu-west-1","eu-west-2","eu-west-3","eu-north-1","ap-east-1","ap-northeast-1","ap-northeast-2","ap-southeast-1","ap-southeast-2","ap-south-1","sa-east-1"]
+regions = ["eu-central-1"]
 
 # data to put in the csv see https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html to add params
 params = ["InstanceId","ImageId","State","LaunchTime"]
 
 ## add here the instance tags you want to get in the csv see in the aws management console to find out what tags are the instances tags
-tags_list = ["Name","Owner"]
-		
+tags_list = ["Name","Owner","Department","BusinessUnit"]
+
--- list-ec2-instances.py
+++ list-ec2-instances.py
@@ -40,9 +40,28 @@ def describe_all_instances(profiles,regions,params,delimiter,frmt):
                     try:
                         for listInstance in jsonfile:
                             for instance in listInstance:
-                                convert_datetime_object = datetime.strptime(str(instance[4]),'%Y-%m-%dT%H:%M:%S.%fZ')
-                                pretty_datetime_object = convert_datetime_object.strftime(frmt)
-                                json_parsed = [profile,region,instance[0],instance[1][0],instance[2],instance[3]["Name"],pretty_datetime_object]
+                                json_parsed = [profile,region]
+                                print ("DEBUG JSON AVANT BOUCLE",json_parsed)
+                                for info in instance:
+                                    if type(info) == list:
+                                        print ("INFO = LIST", info)
+                                        json_parsed.append(info[0])
+                                    elif type(info) == str:
+                                        try:
+                                            print("INFO DATETIME",info)
+                                            convert_datetime_object = datetime.strptime(str(info),'%Y-%m-%dT%H:%M:%S.%fZ')
+                                            pretty_datetime_object = convert_datetime_object.strftime(frmt)
+                                            json_parsed.append(pretty_datetime_object)
+                                        except ValueError:
+                                            json_parsed.append(info)
+                                    elif type(info) == dict:
+                                        try:
+                                            json_parsed.append(info["Name"])
+                                        except KeyError:
+                                            json_parsed.append(info)
+                                    else:
+                                        json_parsed.append(info)
+                                print(json_parsed)
                                 csv_writer.writerow(json_parsed)
                     except IndexError:
                         print ("There are no existing instances for {0} account on region {1}".format(profile,region))
@@ -50,78 +69,12 @@ def describe_all_instances(profiles,regions,params,delimiter,frmt):
         print ("Deleting all json file from folder")
         [os.remove(json) for json in os.listdir(os.path.join(path,'aws-instances-details/all-instances')) if json[-4:] == "json"]
 
-def describe_stopped_instances(profiles,regions,params,delimiter,frmt):
-	print ("Getting all stopped instances")
-	os.chdir(os.path.join(path,'aws-instances-details/stopped-instances'))
-	header = ["Profile","Region"]
-	for param in params:
-		header.append(param)
-	for profile in profiles:
-		for region in regions:
-			print ("getting {0} instances from region {1}".format(profile,region))
-			os.system('aws ec2 --profile {0} --region {1} describe-instances --filters "Name=instance-state-name,Values=stopped" --query "Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value,ImageId,State,LaunchTime"] --output json > {0}_{1}.json'.format(profile,region))		
-	with open("aws-stopped-instance-details.csv","w",newline='') as file:
-		csv_writer = csv.writer(file,delimiter=delimiter)
-		csv_writer.writerow(header)
-		for profile in profiles:
-			for region in regions:
-				print ("Opening {0} region {1} json file".format(profile,region))
-				with open("{0}_{1}.json".format(profile,region),"r") as file1:
-					jsonfile = json.loads(file1.read())
-					try:
-						for listInstance in jsonfile:
-							for instance in listInstance:
-								convert_datetime_object = datetime.strptime(str(instance[4]),'%Y-%m-%dT%H:%M:%S.%fZ')
-								pretty_datetime_object = convert_datetime_object.strftime(frmt)
-								json_parsed = [profile,region,instance[0],instance[1][0],instance[2],instance[3]["Name"],pretty_datetime_object]
-								csv_writer.writerow(json_parsed)
-					except IndexError:
-						print ("There are no existing instances for {0} account on region {1}".format(profile,region))
-						continue
-		print ("Deleting all json file from folder")
-		[os.remove(json) for json in os.listdir(os.path.join(path,'aws-instances-details/all-instances')) if json[-4:] == "json"]
-		
-def describe_running_instances(profiles,regions,params,delimiter,frmt):
-	print ("Getting all running instances")
-	os.chdir(os.path.join(path,'aws-instances-details/running-instances'))
-	header = ["Profile","Region"]
-	for param in params:
-		header.append(param)
-	for profile in profiles:
-		for region in regions:
-			print ("getting {0} instances from region {1}".format(profile,region))
-			os.system('aws ec2 --profile {0} --region {1} describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value,ImageId,State,LaunchTime"] --output json > {0}_{1}.json'.format(profile,region))			
-	with open("aws-running-instance-details.csv","w",newline='') as file:
-		csv_writer = csv.writer(file,delimiter=delimiter)
-		csv_writer.writerow(header)
-		for profile in profiles:
-			for region in regions:
-				print ("Opening {0} region {1} json file".format(profile,region))
-				with open("{0}_{1}.json".format(profile,region),"r") as file1:
-					jsonfile = json.loads(file1.read())
-					try:
-						for listInstance in jsonfile:
-							for instance in listInstance:
-								convert_datetime_object = datetime.strptime(str(instance[4]),'%Y-%m-%dT%H:%M:%S.%fZ')
-								pretty_datetime_object = convert_datetime_object.strftime(frmt)
-								json_parsed = [profile,region,instance[0],instance[1][0],instance[2],instance[3]["Name"],pretty_datetime_object]
-								csv_writer.writerow(json_parsed)
-					except IndexError:
-						print ("There are no existing instances for {0} account on region {1}".format(profile,region))
-						continue
-		print ("Deleting all json file from folder")
-		[os.remove(json) for json in os.listdir(os.path.join(path,'aws-instances-details/all-instances')) if json[-4:] == "json"]
-	
 if __name__ == "__main__":
 	path = os.path.dirname(os.path.abspath(__file__))
 	try:
 		os.mkdir(os.path.join(path, "aws-instances-details"))
-		os.mkdir(os.path.join(path, "aws-instances-details/running-instances"))	
-		os.mkdir(os.path.join(path, "aws-instances-details/stopped-instances"))	
 		os.mkdir(os.path.join(path, "aws-instances-details/all-instances"))	
 	except:
 		print("Folder already exists")
-		
+
 	describe_all_instances(profiles,regions,params,delimiter,frmt)
-	# describe_running_instances(profiles,regions,params,delimiter,frmt)
-	# describe_stopped_instances(profiles,regions,params,delimiter,frmt)
